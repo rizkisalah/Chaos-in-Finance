152755451b85e3e286336dd50b5fb8a61192eace
rizkisalah <152160722+rizkisalah@users.noreply.github.com>
2023-12-22 16:00:57 +0100

    Update and rename test.py to wolf_test.py

--- wolf_test.py
+++ wolf_test.py
@@ -4,7 +4,7 @@ import openpyxl
 data=pd.read_excel(r"C:\Users\salah\OneDrive\Bureau\Projet\Rentabilites_quotidiennes\PEUGEOTTEST.xlsx")
 df = pd.DataFrame(data)
 print(df)
-#print(df["0066353235828410795"])
+
 bdserie=df["Rentabilité"].tolist()
 
 print("L'exposant de Lyapunov vaut : " ,explyap(bdserie,7, 2,10e-3))
